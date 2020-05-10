3ff3c24e69af74f5d05b8a0a5ae4f2324f7b4753
root <root@covid.c3sl.ufpr.br>
2020-05-10 10:29:16 -0300

    adicionada news David Menotti

---
+++ components/News.tsx
@@ -0,0 +1,33 @@
+import {
+    Box,
+    Heading,
+    Divider,
+    Link,
+    Icon
+} from "@chakra-ui/core";
+
+const News = () => {
+    return (
+
+            <Box rounded = 'lg' boxShadow='md' bg="#ffffff" maxW="3xl" mx="auto" p={4}>
+                <Heading size="sm" textAlign="center">
+                    Novidades
+                </Heading>
+                <Divider />
+                <Link href="https://ricmais.com.br/videos/parana-no-ar/diagnostico-do-coronavirus-exame-de-raio-x-identifica-doenca/" isExternal>
+                    <Icon name="chevron-right" mx="2px" /> Diagnóstico do Corona Vírus por Raio-X - professores UFPR e UFOP <Icon name="external-link" mx="2px" />
+                </Link> <br/>
+                <Link href="https://www.youtube.com/watch?v=4HHOuDnW2DQ&feature=emb_logo&fbclid=IwAR3vxB3jC2TkUP9hWTp-9fEqgQ6xlOFKLSRTll5fj6NOjZqtpUzTrAX9tw8" isExternal>
+                    <Icon name="chevron-right" mx="2px" /> Painel da SBF: Físicos e a Pandemia <Icon name="external-link" mx="2px" />
+                </Link> <br/>
+                <Link href="https://www.insper.edu.br/noticias/covid-19-portal-marco-antonio/" isExternal>
+                    <Icon name="chevron-right" mx="2px" /> Professor do Insper colabora com a UFPR em portal sobre a Covid-19 <Icon name="external-link" mx="2px" />
+                </Link> <br/>
+		
+        </Box>
+    );
+};
+
+export default News;
+
+
