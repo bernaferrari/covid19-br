d69bb856c975b403ebdda5d43426456d53135d38
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-06-05 19:38:42 -0300

    fix: restore purple theme and shadcn font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Geist, Geist_Mono } from "next/font/google";
 import { useMemo } from "react";
 import { generateDefaultSeo } from "next-seo/pages";
+import { cn } from "@/lib/utils";
 import seo from "../seo.config";
 import "../styles/globals.css";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   const defaultSeoTags = useMemo(() => generateDefaultSeo(seo), []);
 
   return (
-    <>
+    <div className={cn("font-sans antialiased", geist.variable, geistMono.variable)}>
       <Head>
         {defaultSeoTags}
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -17,7 +29,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel="apple-touch-icon" href="/apple-icon.png" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 };
 
